b6e565c447d28940dc03c46b8feedb4a248499ba
sinkovitsd <sinkovitsd@uwstout.edu>
2020-02-12 13:50:41 -0600

    This is a stub for Particle.py to get you started.

---
+++ Particle.py
@@ -0,0 +1,18 @@
+# import Vec2 here
+
+class Particle(self): # add arguments with default values for each
+    # define object variables and set them equal to the arguments
+    pass # erase all pass statements when you add other code
+
+def clear_force(self):
+    # clear the force variable
+    pass 
+
+def add_force(self, force):
+    # add force to the force variable
+    pass 
+
+def update(self, dt):
+    # update velocity first
+    # update position second
+    pass
